10e5c0895991acfa55892fd377cbd178f428d018
Ratmir Kremcheev <ratmir.kremcheev@gmail.com>
2018-09-11 00:41:51 +0300

    GroupCreationTests class added

---
+++ AddressbookWebTests/AddressbookWebTests/GroupCreationTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AddressbookWebTests
+{
+    [TestFixture]
+    public class GroupCreationTests
+    {
+        private IWebDriver _driver;
+        private string _baseUrl;
+
+        [SetUp]
+        public void SetUpTests()
+        {
+            _driver = new ChromeDriver();
+            _baseUrl = "http://localhost";
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void GroupCreationTest()
+        {
+            _driver.Navigate().GoToUrl(_baseUrl + "/addressbook");
+            _driver.FindElement(By.Name("user")).Clear();
+            _driver.FindElement(By.Name("user")).SendKeys("admin");
+        }
+    }
+}

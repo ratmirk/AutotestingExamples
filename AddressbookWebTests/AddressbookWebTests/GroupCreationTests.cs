7739391b0d4064e13251c0a9633fa89ef7d373b7
Ratmir Kremcheev <ratmir.kremcheev@arcadia.spb.ru>
2018-09-12 09:56:12 +0300

    Added steps to GroupCreationTest

--- AddressbookWebTests/AddressbookWebTests/GroupCreationTests.cs
+++ AddressbookWebTests/AddressbookWebTests/GroupCreationTests.cs
@@ -24,10 +24,25 @@ namespace AddressbookWebTests
         public void GroupCreationTest()
         {
             _driver.Navigate().GoToUrl(_baseUrl + "/addressbook");
+
             _driver.FindElement(By.Name("user")).Clear();
             _driver.FindElement(By.Name("user")).SendKeys("admin");
             _driver.FindElement(By.CssSelector("input[type = password]")).SendKeys("secret");
             _driver.FindElement(By.CssSelector("input[type = submit][value = Login]")).Click();
+
+            _driver.FindElement(By.CssSelector("a[href *= group]")).Click();
+
+            _driver.FindElement(By.Name("new")).Click();
+
+            _driver.FindElement(By.Name("group_name")).SendKeys("group_name");
+            _driver.FindElement(By.Name("group_header")).SendKeys("group_header");
+            _driver.FindElement(By.Name("group_footer")).SendKeys("group_footer");
+
+            _driver.FindElement(By.CssSelector("input[type = submit][name = submit]")).Click();
+
+            _driver.FindElement(By.CssSelector("div[class = msgbox] > i > a[href *= group]")).Click();
+
+
         }
     }
 }

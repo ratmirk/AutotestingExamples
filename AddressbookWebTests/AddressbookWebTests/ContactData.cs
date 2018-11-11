2a5a6b72e7a62a445a98463c2b6e69a6ef83d971
Ratmir Kremcheev <ratmir.kremcheev@gmail.com>
2018-11-11 13:48:24 +0300

    Added Contact Data class

---
+++ AddressbookWebTests/AddressbookWebTests/ContactData.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressbookWebTests
+{
+    internal class ContactData
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+    }
+}

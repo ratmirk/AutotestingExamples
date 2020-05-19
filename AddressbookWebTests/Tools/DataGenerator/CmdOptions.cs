c9b9aeca9dd9dcea404bd4d2fa1285730191990d
Ratmir Kremcheev <ratmir.kremcheev@arcadia.spb.ru>
2020-05-20 00:52:26 +0300

    DataGenerator added

---
+++ AddressbookWebTests/Tools/DataGenerator/CmdOptions.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+namespace DataGenerator
+{
+    public class CmdOptions
+    {
+        [Option('t', "dataType", Required = true, HelpText = "Data type: contacts or groups")]
+        public string DataType { get; set; }
+
+        [Option('c', "count")]
+        public string Count { get; set; } = "1";
+
+        [Option('f', "fileName", Required = true, HelpText = "Data type: contacts or groups")]
+        public string FileName { get; set; }
+
+        [Option('d', "dataFormat", Required = true, HelpText = "Data format: csv, xml or JSON")]
+        public string DataFormat { get; set; }
+    }
+}
---
+++ AddressbookWebTests/Tools/DataGenerator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using AddressbookWebTests;
+using CommandLine;
+
+namespace DataGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CmdOptions>(args).WithParsed(RunOptions);
+        }
+
+        static void RunOptions(CmdOptions opts)
+        {
+            var count = int.Parse(opts.Count);
+            using (var sw = new StreamWriter(opts.FileName))
+            {
+                switch (opts.DataFormat)
+                {
+                    case "csv":
+                    { 
+                        for (int i = 0; i < count; i++)
+                        {
+                            sw.WriteLine($"{Randomizer.GenerateRandomString(7)}," +
+                                         $"{Randomizer.GenerateRandomString(7)}," +
+                                         $"{Randomizer.GenerateRandomString(7)}");
+                        }
+                        break;
+                    }
+                    case "xml":
+                        // TODO
+                        break;
+                    case "JSON":
+                        // TODO
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid format");
+                }
+            }
+        }
+    }
+}

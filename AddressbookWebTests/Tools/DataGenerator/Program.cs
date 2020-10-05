85f92865be6665c0553b4f3d51eeb7656d6e59f1
Ratmir Kremcheev <ratmir.kremcheev@arcadia.spb.ru>
2020-10-06 00:30:17 +0300

    DataGenerator improvements

--- AddressbookWebTests/Tools/DataGenerator/Program.cs
+++ AddressbookWebTests/Tools/DataGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using AddressbookWebTests;
 using CommandLine;
@@ -10,6 +11,7 @@ namespace DataGenerator
         static void Main(string[] args)
         {
             Parser.Default.ParseArguments<CmdOptions>(args).WithParsed(RunOptions);
+            Console.ReadKey();
         }
 
         static void RunOptions(CmdOptions opts)
@@ -17,7 +19,7 @@ namespace DataGenerator
             var count = int.Parse(opts.Count);
             using (var sw = new StreamWriter(opts.FileName))
             {
-                switch (opts.DataFormat)
+                switch (opts.DataFormat.ToLower())
                 {
                     case "csv":
                     { 
@@ -32,13 +34,42 @@ namespace DataGenerator
                     case "xml":
                         // TODO
                         break;
-                    case "JSON":
+                    case "json":
                         // TODO
                         break;
                     default:
-                        throw new ArgumentException("Invalid format");
+                        Console.WriteLine("Invalid format. Type --help to display the help screen.");
+                        break;
                 }
             }
+        }
+
+        static List<ContactData> CreateContactData(int count)
+        {
+            var contactData = new List<ContactData>();
+            for (int i = 0; i < count; i++)
+            {
+                contactData.Add(new ContactData
+                {
+                    FirstName = Randomizer.GenerateRandomString(7),
+                    LastName = Randomizer.GenerateRandomString(7),
+                    MiddleName = Randomizer.GenerateRandomString(7),
+                    Email = Randomizer.GenerateRandomString(7),
+                    NickName = Randomizer.GenerateRandomString(7),
+                });
+            }
+            return contactData;
+        }
+        static List<GroupData> CreateGroupData(int count)
+        {
+            var groupData = new List<GroupData>();
+            for (int i = 0; i < count; i++)
+            {
+                groupData.Add(new GroupData(Randomizer.GenerateRandomString(7),
+                    Randomizer.GenerateRandomString(7),
+                    Randomizer.GenerateRandomString(7)));
+            }
+            return groupData;
         }
     }
 }
